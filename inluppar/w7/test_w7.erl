24c04df480bdbd473574c7a2d4643f60a4538d61
kristian <kristian.lundstrom@gmail.com>
2014-02-16 23:27:32 +0100

    nn

--- inluppar/w7/test_w7.erl
+++ inluppar/w7/test_w7.erl
@@ -4,3 +4,10 @@
 t1() ->
 	[{port, 200}, {timer, 200}] = w7:get_config("config.txt", [{port, 400}, {timer, 200}]),
 	awesome.
+	
+t2() ->
+	[{1,<<"{port,200}.\r\n">>},
+	{2,<<"{hostname,\"www.goole.com\"}.\r\n">>},
+	{3,<<"{interval,10}.">>}] = w7:read_and_number_lines("config.txt"),
+	 good.
+	 
--- inluppar/w7/w7.erl
+++ inluppar/w7/w7.erl
@@ -5,6 +5,20 @@ file_to_list(File) ->
 	{ok, Bin} = file:read_file(File),
 	binary_to_list(Bin).
 	
+read_and_number_lines(File) ->
+	Text = file_to_list(File),
+	make_row_tuples(1, Text, [], []).
+	
+make_row_tuples(N, "\n"++T, R, BigR) ->
+	make_row_tuples(N+1, T, [], [{N, list_to_binary(lists:reverse("\n"++R))}|BigR]);
+	
+make_row_tuples(N, [H|T], R, BigR) ->
+	make_row_tuples(N, T, [H|R], BigR);
+
+make_row_tuples(N, [], R, BigR) ->
+	lists:reverse([{N, list_to_binary(lists:reverse(R))}|BigR]).
+	
+
 	
 get_config(F, Args) -> 
 	L = consult(F),
@@ -26,13 +40,24 @@ match([{K, V}|_], {Key, _}) when K == Key ->
 
 match([_|T], Tuple) ->
 	match(T, Tuple).
-		
-
-		
-
 
 consult(File) ->
 	{ok, L} = file:consult(File),
 	L.
+	
+list_dir(Dir) ->
+	{ok, L} = file:list_dir(Dir),
+	make_file_tuples(L, []).
+	
+make_file_tuples([], R) ->
+	lists:reverse(R);
+make_file_tuples([H|T], R) ->
+	case filelib:is_dir(H) of 
+		false ->
+			make_file_tuples(T, [{file, H, filelib:file_size(H)}|R]);
+		true ->
+			 make_file_tuples(T, [{dir, H}|R])
+	end.
+	
 	
 	
